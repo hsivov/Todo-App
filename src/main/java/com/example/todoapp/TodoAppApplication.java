41e099c566cb154d33c0c5f2fb652a91dd8d907f
hsivov <hsivov@students.softuni.bg>
2023-02-07 22:08:08 +0200

    Setup deploy configuration

--- src/main/java/com/example/todoapp/TodoAppApplication.java
+++ src/main/java/com/example/todoapp/TodoAppApplication.java
@@ -2,9 +2,10 @@ package com.example.todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TodoAppApplication {
+public class TodoAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoAppApplication.class, args);
