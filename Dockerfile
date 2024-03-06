df8cd4fdd61a02ff345bae91ef9252553bb1f007
hsivov <hsivov@students.softuni.bg>
2024-03-06 20:01:43 +0200

    Update .gitignore

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM eclipse-temurin:17-alpine
-WORKDIR /app
-COPY target/todo-app.jar .
-CMD ["java", "-jar", "todo-app.jar"]
--- src/main/java/com/example/todoapp/repository/TodoItemRepository.java
+++ src/main/java/com/example/todoapp/repository/TodoItemRepository.java
@@ -1,6 +1,5 @@
 package com.example.todoapp.repository;
 
-import com.example.todoapp.model.dto.TodoItemDTO;
 import com.example.todoapp.model.entity.TodoItem;
 import com.example.todoapp.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
